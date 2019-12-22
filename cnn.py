e772a28e56aecec1c3e7dc873745a20dd2cd5db8
samnaniaahil <37119335+samnaniaahil@users.noreply.github.com>
2019-12-22 15:28:52 -0500

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -1,14 +1,5 @@
 # Convolutional Neural Network
 
-# Installing Theano
-# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
-
-# Installing Tensorflow
-# pip install tensorflow
-
-# Installing Keras
-# pip install --upgrade keras
-
 # Part 1 - Building the CNN
 
 # Importing Keras libraries and packages
